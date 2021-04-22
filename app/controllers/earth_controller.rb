91aebdba7d00457310ec64db63c91e3ad914cdcb
Hammad-Rahman <hammad.rahman@chicagobooth.edu>
2021-04-22 21:31:44 +0000

    on cancer

--- app/controllers/earth_controller.rb
+++ app/controllers/earth_controller.rb
@@ -13,6 +13,8 @@ class EarthController < ApplicationController
     render({ :template => "nature_templates/taurus.html.erb" })
   end
 
+
+
   def maiden
     @horoscope = "Hard work, enthusiasm, and dedication are now paying off financially, Virgo. New opportunities are opening up to profitably make use of your artistic side. The downside is that you might be working so hard that you're too drained to be creative even though the inspiration is there. 
     Take a moment to rework your schedule to accomplish the most in the least amount of time. You can do it."
@@ -28,6 +30,8 @@ class EarthController < ApplicationController
     render({ :template => "nature_templates/virgo.html.erb" })
   end
 
+
+  
   def goat
     @horoscope = "You could be on a real emotional high today because of your success and that of other household members, Capricorn. Your mind may be buzzing with ideas for future expansion, some of which may not be all that workable. However, you should allow yourself a few flights of fancy. Tomorrow your feet will be back on the ground and you will see things in a more practical light."
 
