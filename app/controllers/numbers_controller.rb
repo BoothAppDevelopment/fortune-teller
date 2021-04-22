c13d003faac4617b3494c82c8c2315ddc6f218b0
Hammad-Rahman <hammad.rahman@chicagobooth.edu>
2021-04-22 20:13:15 +0000

    unlucky done

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,17 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+    def unlucky
+    @random_array = Array.new
+
+    5.times do
+      number = rand(1...100)
+      
+      @random_array.push(number)
+    end
+
+    render({ :template => "lottery_stuff/unlucky.html.erb"})
+  end 
+
 end
